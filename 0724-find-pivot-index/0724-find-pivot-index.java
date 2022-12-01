b3851ab36903cdad0ea12692850c90ad2f53632c
kimchan <55445564+kimchan123@users.noreply.github.com>
2022-12-01 20:23:01 +0900

    Time: 382 ms (12.89%), Space: 42.9 MB (92.55%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            int left = 0, right = 0;
+            
+            for (int j=0; j<i; j++) {
+                left += nums[j];        
+            }
+            
+            for (int j=i+1; j<nums.length; j++) {
+                right += nums[j];        
+            }
+            
+            if (left == right) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
