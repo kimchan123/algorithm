d7f889d90d69e7de734613042a906708a60ee2f1
kimchan <55445564+kimchan123@users.noreply.github.com>
2023-01-15 14:32:13 +0900

    Time: 2 ms (78.50%), Space: 58.9 MB (80.57%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int profit = 0;
+        int buyPrice = prices[0];
+        
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i] - buyPrice);
+            }
+        }
+        
+        return profit;
+    }
+}
